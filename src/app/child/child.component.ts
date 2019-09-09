e80b2f83a121b31439470be2550bcecf535e9289
Vivekanand P V <vivekanandpv@outlook.com>
2019-09-09 06:44:22 +0530

    ngDoCheck and ngAfterViewInit

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter,
+  DoCheck,
+  AfterViewInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, DoCheck, AfterViewInit {
   @Output() customEvent = new EventEmitter();
 
-  constructor() {}
+  constructor() {
+    console.log('constructor called');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
 
   clickHandler() {
     console.log('Click handler in child called');
